191465e097aa8c89ea224fab4fbe5120cd1c6615
pponnek <rbudi6823@gmail.com>
2026-01-16 13:37:50 +0700

    feat: auth jwt

--- internal/routes/auth_route.go
+++ internal/routes/auth_route.go
@@ -1,19 +1,34 @@
 package routes
 
 import (
-	"net/http"
+	"mikrotikapp/internal/config"
+	"mikrotikapp/internal/domain/auth"
+	"mikrotikapp/internal/domain/repositories"
+	"mikrotikapp/internal/domain/services"
+	"mikrotikapp/pkg/database"
+	"mikrotikapp/pkg/middleware"
 
 	"github.com/labstack/echo/v4"
 )
 
 func AuthRoutes(e *echo.Group) {
+
+	cfg := config.Load()
+
+	db := database.Connect(cfg.DBurl)
+
+	userRepo := repositories.NewUserRepository(db)
+	authService := services.NewAuthServcie(userRepo)
+	authController := auth.NewAuthController(authService)
+
 	auth := e.Group("/auth")
-	{
-		auth.GET("/login", func(c echo.Context) error {
-			return c.String(http.StatusOK, "/login")
-		})
-		auth.GET("/register", func(c echo.Context) error {
-			return c.String(http.StatusOK, "/register")
-		})
-	}
+
+	auth.POST("/register", authController.Register)
+	auth.POST("/login", authController.Login)
+
+	protected := auth.Group("")
+	protected.Use(middleware.JWTMiddleware)
+
+	protected.GET("/profile", authController.Profile)
+
 }
